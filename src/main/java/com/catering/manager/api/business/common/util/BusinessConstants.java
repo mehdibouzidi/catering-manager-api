8665ffc2de3555d980fc344e88ee11cae436e1f9
Mehdi BOUZIDI <mehdi.bouzidi.92@gmail.com>
2024-06-07 18:02:22 +0200

    [TECH] Refacto tables

--- src/main/java/com/catering/manager/api/business/common/util/BusinessConstants.java
+++ src/main/java/com/catering/manager/api/business/common/util/BusinessConstants.java
@@ -4,9 +4,9 @@ public class BusinessConstants {
 
     //Database Constants
     public static final String BUSINESS_SCH = "business";
-    public static final String CATEGORY_TABLE = "category";
-    public static final String SUB_CATEGORY_TABLE = "sub_category";
-    public static final String INGREDIENT_TABLE = "ingredient";
+    public static final String CATEGORY_TABLE = "data_category";
+    public static final String SUB_CATEGORY_TABLE = "data_sub_category";
+    public static final String INGREDIENT_TABLE = "data_ingredient";
 
     // REST Constants
     public static final String CATEGORY_CONTROLLER = "category";
